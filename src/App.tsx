520692f7ab362c387f61514334cf02727f437586
Diego <119649331+818diego@users.noreply.github.com>
2024-12-28 13:03:38 -0800

    update import analytucs vercel

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { Projects } from './components/sections/Projects';
 import { Skills } from './components/sections/Skills';
 import { Footer } from './components/Footer';
 import { ScrollProgress } from './components/ScrollProgress';
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -21,7 +20,6 @@ function App() {
       <Experience />
       <Projects />
       <Skills />
-      <Analytics />
       <Footer />
     </div>
   );
