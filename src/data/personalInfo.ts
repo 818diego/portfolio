484321ce66ee3bcadea3d4ca2c1e313cbf0b3712
Diego <119649331+818diego@users.noreply.github.com>
2026-02-01 19:32:29 -0800

    fix:  issue where the description was not translated into Spanish/English has been fixed.

--- src/data/personalInfo.ts
+++ src/data/personalInfo.ts
@@ -12,7 +12,7 @@ export const personalInfo = () => {
     social: {
       github: "https://github.com/818diego",
       linkedin: "https://www.linkedin.com/in/818diego/",
-      cv: "DiegoCV.pdf"
+      cv: "DiegoMerinoCV.pdf"
     }
   };
 };
